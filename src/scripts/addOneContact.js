ea9910171d8cb87fc1b453ed581c0da57225422a
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-05-18 12:10:07 +0300

    add-oneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,11 +1,25 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/constants.js';
 import { createFakeContact } from '../createFakeContact/createFakeContact.js';
 
 export const addOneContact = async () => {
   const oneContact = createFakeContact();
 
+  let data;
+
   try {
-    await fs.writeFile('output.txt', oneContact, 'utf8');
+    data = await fs.readFile(PATH_DB);
+    console.log('Вміст файлу:', JSON.parse(data));
+  } catch (err) {
+    console.error('Помилка читання файлу:', err);
+  }
+
+  const newContactsArr = JSON.parse(data);
+  newContactsArr.push(oneContact);
+  const newData = JSON.stringify(newContactsArr);
+
+  try {
+    await fs.writeFile(PATH_DB, newData);
     console.log('Дані успішно записані у файл.');
   } catch (err) {
     console.error('Помилка запису у файл:', err);
