35fe75e6d4284fb6e6a5d486d6974b6658f62cd1
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-05-18 11:48:57 +0300

    фвв-generate

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,15 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { PATH_DB } from '../constants/constants.js';
+import { createFakeContact } from '../createFakeContact/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  const oneContact = createFakeContact();
+
+  try {
+    await fs.writeFile('output.txt', oneContact, 'utf8');
+    console.log('Дані успішно записані у файл.');
+  } catch (err) {
+    console.error('Помилка запису у файл:', err);
+  }
+};
 
 await addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -3,18 +3,19 @@ import { PATH_DB } from '../constants/constants.js';
 import { createFakeContact } from '../createFakeContact/createFakeContact.js';
 
 const generateContacts = async (number) => {
-  console.log(PATH_DB);
   let contactsArr = [];
   for (let i = 0; i < number; i++) {
     contactsArr.push(createFakeContact());
   }
-  fs.writeFile(PATH_DB.parse, contactsArr);
-  //   try {
-  //     await fs.writeFile(PATH_DB, contactsArr);
-  //     console.log('Дані успішно записані у файл.');
-  //   } catch (err) {
-  //     console.error('Помилка запису у файл:', err);
-  //   }
+
+  const data = JSON.stringify(contactsArr);
+
+  try {
+    await fs.writeFile(PATH_DB, data);
+    console.log('Дані успішно записані у файл.');
+  } catch (err) {
+    console.error('Помилка запису у файл:', err);
+  }
 };
 
 await generateContacts(5);
