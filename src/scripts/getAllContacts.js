7c77ff88954967ba38dbd2ce41082f232a2f1fbe
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-05-18 12:45:46 +0300

    fix

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -4,7 +4,7 @@ import { PATH_DB } from '../constants/constants.js';
 export const getAllContacts = async () => {
   try {
     const data = await fs.readFile(PATH_DB);
-    console.log('Вміст файлу:', JSON.parse(data));
+    return JSON.parse(data);
   } catch (err) {
     console.error('Помилка читання файлу:', err);
   }
