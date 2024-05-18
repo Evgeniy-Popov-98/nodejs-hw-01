4646e2cf9233616c9df823a985ebebfb0df43038
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-05-18 12:43:23 +0300

    add

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,13 @@
-import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { PATH_DB } from '../constants/constants.js';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB);
+    console.log('Вміст файлу:', JSON.parse(data));
+  } catch (err) {
+    console.error('Помилка читання файлу:', err);
+  }
+};
 
 console.log(await getAllContacts());
--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,23 @@
-import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { PATH_DB } from '../constants/constants.js';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  let contactsArr;
+  try {
+    const data = await fs.readFile(PATH_DB);
+    contactsArr = JSON.parse(data);
+  } catch (err) {
+    console.error('Помилка читання файлу:', err);
+  }
+
+  let newContactsArr = [];
+
+  for (let i = 0; i < contactsArr.length; i++) {
+    if (Math.random() >= 0.5) {
+      newContactsArr.push(contactsArr[i]);
+    }
+  }
+  console.log(newContactsArr.length);
+};
 
 await thanos();
