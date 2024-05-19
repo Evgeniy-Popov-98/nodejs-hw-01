e2f1901ef046212f3f9937ce08481389630d0331
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-05-19 15:32:24 +0300

    Update generateContacts.js

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -3,28 +3,17 @@ import { PATH_DB } from '../constants/constants.js';
 import { createFakeContact } from '../createFakeContact/createFakeContact.js';
 
 const generateContacts = async (number) => {
-  let contactsArr = [];
-
   try {
     const data = await fs.readFile(PATH_DB);
     contactsArr = JSON.parse(data);
-  } catch (err) {
-    console.error('Помилка читання файлу:', err);
-  }
-
-  const numberArr = number + contactsArr.length;
-
-  for (let i = contactsArr.length; i < numberArr; i++) {
+    for (let i = 0; i < number; i += 1) {
     contactsArr.push(createFakeContact());
-  }
-
-  const data = JSON.stringify(contactsArr);
-
-  try {
-    await fs.writeFile(PATH_DB, data);
-    console.log('Дані успішно записані у файл.');
+   }
+  const newData = JSON.stringify(contactsArr, null, 2);
+   await fs.writeFile(PATH_DB, newData);
+   console.log('Дані успішно записані у файл.');
   } catch (err) {
-    console.error('Помилка запису у файл:', err);
+    console.error(err);
   }
 };
 
